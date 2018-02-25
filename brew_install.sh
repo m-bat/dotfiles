9007b645da76a038c63e275f5b82ec34e442e668
m-bat <manabu.protcol@gmail.com>
2018-02-25 19:39:06 +0900

    Update .zshrc and Add brew_install.sh

---
+++ brew_install.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+echo "installing homebrew..."
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "run brew doctor..."
+which brew >/dev/null 2>&1 && brew doctor
+
+echo "run brew update..."
+which brew >/dev/null 2>&1 && brew update
+
+echo "ok. run brew upgrade..."
+
+brew upgrade --all
+
+formulas=(
+    git
+    wget
+    curl
+    tree
+    openssl
+    "--without-etcdir zsh"
+    zsh-completions
+    emacs
+    cask
+    ansible
+    python3
+    markdown
+    ctags
+    ssh-copy-id
+)
+
+
+echo "start brew install apps..."
+for formula in "${formulas[@]}"; do
+    brew install $formula || brew upgrade $formula
+done
+
+casks=(
+    iterm2
+    virtualbox
+    vagrant
+    vagrant-manager
+)
+
+echo "start brew cask install apps..."
+for cask in "${casks[@]}"; do
+    brew cask install $cask
+done
+
+brew cleanup
+brew cask cleanup
+
+cat << END
+
+**************************************************
+HOMEBREW INSTALLED! bye.
+**************************************************
+
+END
